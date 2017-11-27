9e1b667d6cd47a889e380f65953c5903f8e9801c
utubo <utbutb@gmail.com>
2017-11-27 14:17:22 +0900

    apt-check.shの結果を標準出力にも表示

--- apt-check.sh
+++ apt-check.sh
@@ -3,6 +3,7 @@
 apt-get update
 chk=$(apt-get -s upgrade | grep -C 1 以下のパッケージ | grep -v 検出しています)
 if [ -z "$chk" ]; then
+  echo 更新が必要なパッケージはありません。
   if [ -e /var/login_msg.d/apt-check.txt ]; then
     rm /var/login_msg.d/apt-check.txt
   fi
@@ -10,7 +11,9 @@ else
   if [ ! -d /var/login_msg.d ]; then
     mkdir -p /var/login_msg.d
   fi
-  echo 更新が必要なパッケージがあります。 > /var/login_msg.d/apt-check.txt
-  echo "$chk" >> /var/login_msg.d/apt-check.txt
+  logfile=/var/login_msg.d/apt-check.txt
+  echo 更新が必要なパッケージがあります。 > $logfile
+  echo "$chk" >> $logfile
+  cat $logfile
 fi
 
